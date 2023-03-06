81f0b6a42ed24f255aa645652a13eb872e0b6a61
Birger Möllering <bmoellering@googlemail.com>
2023-03-06 18:11:46 +0100

    einheitliches entprellen

--- bewegungsmelder_test.py
+++ bewegungsmelder_test.py
@@ -20,6 +20,9 @@ import RPi.GPIO as gpio
 # Das Script aus dem Netz verwendet BCM !
 # D.h. es ist möglich, dass du das falsch gesteckt hast!
 
+# entprellen des Sensors
+BOUNCETIME=300
+
 def ctrl_c_handler(sig, frame):
     # gpio.cleanup() wurde in dem anderen Script vergessen!
     # wenn gpio.cleanup() nicht aufgerufen wird,
@@ -29,12 +32,12 @@ def ctrl_c_handler(sig, frame):
 
 def rising_handler(pin):
     gpio.remove_event_detect(pin)
-    gpio.add_event_detect(pin, gpio.FALLING, callback=falling_handler, bouncetime=200)
+    gpio.add_event_detect(pin, gpio.FALLING, callback=falling_handler, bouncetime=BOUNCETIME)
     print(datetime.datetime.now().time(), "angegangen")
 
 def falling_handler(pin):
     gpio.remove_event_detect(pin)
-    gpio.add_event_detect(pin, gpio.RISING, callback=rising_handler, bouncetime=200)
+    gpio.add_event_detect(pin, gpio.RISING, callback=rising_handler, bouncetime=BOUNCETIME)
     print(datetime.datetime.now().time(), "ausgegangen")
 
 if __name__ == "__main__":
@@ -46,8 +49,8 @@ if __name__ == "__main__":
     gpio.setup(PIR_PIN, gpio.IN)
 
     if gpio.input(PIR_PIN):
-        gpio.add_event_detect(PIR_PIN, gpio.FALLING, callback=falling_handler, bouncetime=200)
-    else: gpio.add_event_detect(PIR_PIN, gpio.RISING, callback=rising_handler, bouncetime=200)
+        gpio.add_event_detect(PIR_PIN, gpio.FALLING, callback=falling_handler, bouncetime=BOUNCETIME)
+    else: gpio.add_event_detect(PIR_PIN, gpio.RISING, callback=rising_handler, bouncetime=BOUNCETIME)
 
     while True:
         time.sleep(0.1)
